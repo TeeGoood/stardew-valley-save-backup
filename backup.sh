81fc8667aec115546f7d86bacc77c2c20ba0ed8f
teegooodgood <teegooodgood@gmail.com>
2025-12-25 23:00:38 +0700

    feat: create backup per farms

--- backup.sh
+++ backup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-inotifywait -m -e moved_to --include "_old$" ~/.config/StardewValley/Saves/testbackup_426349532 |
+root=~/.config/stardew-valley-save-backups
+
+inotifywait -m -e moved_to --include "_old$" ~/.config/StardewValley/Saves/* |
 while read path action file; do
-  echo "$path $action $file"
+  farm=$(awk -F'/' '{print $(NF-1)}' <<< $path)
+  backups="$root/$farm"
+  mkdir -p $backups
+
   backup=${file%_old}
-  current_backup=$(ls | grep "^$backup" | wc -l)
-  order=$((current_backup+1))
-  cp "${path}${file}" "${backup}_${order}_$(date +%Y%m%d%H%M%S)"
+  total_backups=$(ls $backups | grep ^$backup | wc -l)
+  order=$((total_backups+1))
+
+  cp "${path}${file}" "$root/$farm/${backup}_${order}_$(date +%Y%m%d%H%M)"
 done
